30fca8af678f64295466be60ce6d972a1e00517f
zhoux <123456>
2022-02-26 23:30:48 +0800

    项目初始化

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>新建分类</h1>
+    <el-form>
+      <el-form-item label="名称">
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
