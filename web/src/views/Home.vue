50f53af6093843316a3098ec517077d3d7fc6068
zhoux <123456>
2022-03-02 16:44:15 +0800

    移动端初始化

---
+++ web/src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-slide :options="swiperOptions">
+        <img
+          class="w-100"
+          src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg"
+        />
+      </swiper-slide>
+      <div
+        class="swiper-pagination pagination-home text-right px-3 pb-1"
+        slot="pagination"
+      ></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      swiperOptions: {
+        loop: true,
+        pagination: {
+          el: ".pagination-home",
+        },
+        // Some Swiper option/callback...
+      },
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "../assets/scss/_variable.scss";
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
+</style>
